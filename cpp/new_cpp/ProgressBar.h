a7703fa7a26662f3164c715fa6921c0b8f70e643
jame-parky <72101291+james-parky@users.noreply.github.com>
2022-01-26 20:31:02 +0000

    Added template and reformatted

--- cpp/new_cpp/ProgressBar.h
+++ cpp/new_cpp/ProgressBar.h
@@ -1,2 +1,58 @@
-void drawProgressBar(int increment, int target, int percentage,
-                     bool annotation);
+/*Functions for printing custom progress bars
+
+Author: James Parkington
+License: Prublic Domain
+Version: 1.4
+*/
+
+#include <iostream>
+#include <stdexcept>
+
+#define TARGET_TICK '@'
+#define FILLED_TICK '#'
+#define EMPTY_TICK '-'
+
+#define TARGET_ERROR "Target must be an integer in the range 0-100."
+#define INCREMENT_ERROR "Increment must be an integer, either 1, 2 or 5."
+#define PERCENTAGE_ERROR "Percentage must be an integer in the range 0-100."
+
+void setProgressBar(char* progressBar, int increment, int percentage,
+                    int target) {
+    for (int i = 0; i < 100 / increment; i++) {
+        progressBar[i] = EMPTY_TICK;
+    }
+    int roundedPercentage = percentage - (percentage % increment);
+    for (int i = 0; i < roundedPercentage / increment; i++) {
+        progressBar[i] = FILLED_TICK;
+    }
+    int roundedTarget = target - (target % increment);
+    progressBar[(roundedTarget / increment) - 1] = TARGET_TICK;
+}
+
+namespace PB {
+template <typename T>
+void drawProgressBar(int increment, T target, T percentage, bool b_annotation) {
+    if (!(increment == 1 || increment == 2 || increment == 5)) {
+        throw std::invalid_argument(INCREMENT_ERROR);
+    }
+
+    if (!(target >= (T)0 && target <= (T)100)) {
+        throw std::invalid_argument(TARGET_ERROR);
+    }
+
+    if (!(percentage >= (T)0 && percentage <= (T)100)) {
+        throw std::invalid_argument(PERCENTAGE_ERROR);
+    }
+
+    char progressBar[100 / increment];
+    setProgressBar(progressBar, increment, percentage, target);
+
+    std::cout << '|';
+    for (int i = 0; i < 100 / increment; i++) {
+        std::cout << progressBar[i];
+    }
+    std::cout << '|';
+    if (b_annotation) std::cout << percentage << '%';
+    std::cout << std::endl;
+}
+}  // namespace PB
